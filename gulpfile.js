8ba7f840c1400839a8146dc7585274606baf623b
Badi Labassi <blabassi@aldogroup.com>
2015-07-23 08:56:04 -0400

    Gulp + Theme & Homepage customization

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+// https://github.com/unlight/gulp-cssimport
+var gulp = require('gulp');
+var cssimport = require("gulp-cssimport");
+
+var globalConfig = {
+  src: 'sass' // your dev stylesheet directory. No trailing slash
+};
+
+// Process CSS
+gulp.task('styles', function(){
+  return gulp.src(globalConfig.src + '/**/*.*')
+    .pipe(cssimport())
+    .pipe(gulp.dest('assets/'));
+})
+
+// Watch files
+gulp.task('watch', function () {
+  gulp.watch(globalConfig.src + '/**/*.*', ['styles']);
+});
+
+// Default task
+gulp.task('default', ['styles', 'watch']);
